377927f75118ba7151b73b725441de4349d3241d
shrine <1015545250@qq.com>
2019-04-01 18:51:22 +0800

    adjust api config

--- common/models/ApiConfig.php
+++ common/models/ApiConfig.php
@@ -3,7 +3,7 @@ namespace  common\models;
 
 class ApiConfig
 {
-	const app_id = "defaultid";
-	const app_secret = "default-secret";
+	const app_id = "your_id";
+	const app_secret = "your_secret";
 	const code2session_url = "https://api.weixin.qq.com/sns/" ;//method => GET;
 }
