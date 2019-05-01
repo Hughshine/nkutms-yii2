5abed0302bc4559ce7d7c36ae2a39cd2c377fcd6
Hugh <32846078+Hughshine@users.noreply.github.com>
2019-05-01 21:02:01 +0800

    Delete ApiConfig.php

--- common/models/ApiConfig.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace  common\models;
-
-class ApiConfig
-{
-	const app_id = "wxb58acb9fcc1e6c96";
-	const app_secret = "a0b191969f401c5e38399c6ff93a87a8";
-	const code2session_url = "https://api.weixin.qq.com/sns/" ;//method => GET;
-}
